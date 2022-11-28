b10b55c997dd25e96ae2bbc056ddbd295375b815
antojoel <antojoel8020@gmail.com>
2022-11-28 22:45:07 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,39 @@
+export const projects = [
+  {
+    title: "Nalaiya Vidiyal NGO",
+    subtitle: "Public Relations Website",
+    description:
+      "I have created a website for a NGO called Nalaiya Vidiyal. The website is created using Php and hosted on Veeble. The website is fully responsive and has a dark mode.",
+    image: "./project1.png",
+    link: "http://nalaiyavidiyal.com/",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jesse Hicks",
+    company: "Zoozle",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
+  },
+];
+
+export const skills = [
+  "Python",
+  "Java",
+  "React",
+  "Php",
+  "SQL",
+  "Linux",
+  "Git",
+  "Anything Else",
+
+];
