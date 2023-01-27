798503f5f049a8f8607593daaf5b9fa02dfcc38d
antojoel <antojoel8020@gmail.com>
2023-01-27 12:06:28 +0530

    updated projects

--- src/data.js
+++ src/data.js
@@ -3,9 +3,25 @@ export const projects = [
     title: "Nalaiya Vidiyal NGO",
     subtitle: "Public Relations Website",
     description:
-      "I have created a website for a NGO called Nalaiya Vidiyal. The website is created using Php and hosted on Veeble. The website is fully responsive and has a dark mode.",
+      "I have created a website for a NGO called Nalaiya Vidiyal. The website is created using Php and hosted on Veeble. The website is fully responsive",
     image: "./project1.png",
     link: "http://nalaiyavidiyal.com/",
+  },
+  {
+    title: "Jussimple",
+    subtitle: "Admin Dashboard for Jussimple Trading",
+    description:
+      "I have created a admin dashboard for a trading company called Jussimple Trading. The dashboard is fully responsive due to security reason you are restrictesd to access the page",
+    image: "./project2.png",
+    link: "http://jussimple.com/",
+  },
+  {
+    title: "jewelleriesandbeauty",
+    subtitle: "Ecommerce Website for Jewelleries and Beauty",
+    description:
+      "I have created a ecommerce website for a jewellery and beauty company called Jewelleries and Beauty, which also include payment gateway of paypal and its on progress. The website is fully responsive",
+    image: "./project3.png",
+    link: "http://jewelleriesandbeauty.com/",
   },
 ];
 
